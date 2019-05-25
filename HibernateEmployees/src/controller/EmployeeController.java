7114717a732e6fe2aed30270ee32c943d95543b0
PutraPradana <ppradana92@gmail.com>
2019-05-25 16:15:49 +0700

    Cek

---
+++ HibernateEmployees/src/controller/EmployeeController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import icontroller.IEmployeeController;
+import idaos.IGeneralDAO;
+import daos.GeneralDAO;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import models.Employee;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class EmployeeController implements IEmployeeController{
+    
+    Date date = new Date(); // this object contains the current date value 
+    SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+    private IGeneralDAO<Employee> gdao;
+
+    public EmployeeController(SessionFactory factory) {
+        gdao = new GeneralDAO(factory, Employee.class);
+    }
+    
+    
+    @Override
+    public List<Employee> getAll() {
+        return gdao.getData(gdao);
+    }
+
+    @Override
+    public Employee getById(String id) {
+        return gdao.getById(new Integer(id));
+    }
+
+    @Override
+    public List<Employee> search(Object keyword) {
+        return gdao.getData(keyword);
+    }
+
+   
+    @Override
+    public String delete(String id) {
+        if (gdao.saveOrDelete(new Employee(), true)) {
+            return "Data berhasil dihapus";
+        }else{
+            return "Data gagal dihapus";
+        }
+    }
+
+    @Override
+    public String save(String id, String firstName, String lastName, String email, String phoneNumber, String hireDate, String salary, String commissionPct, String department, String manager, String job) {
+        
+        if (gdao.saveOrDelete(new Employee(), false)) {
+            return "Data berhasil ditambahkan";
+        }else{
+            return "Data gagal ditambahkan";
+        }
+    }
+    
+}
---
+++ HibernateEmployees/src/tools/ManualTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools;
+
+import controller.EmployeeController;
+import daos.GeneralDAO;
+import icontroller.IEmployeeController;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Formatter;
+import models.Department;
+import models.Employee;
+import models.Job;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class ManualTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ParseException {
+        // TODO code application logic here
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        System.out.println(factory);
+
+        GeneralDAO<Employee> edao = new GeneralDAO<>(factory, Employee.class);
+        IEmployeeController eco = new EmployeeController(factory);
+        
+        System.out.println(eco.search("King"));
+//        System.out.println(eco.getById("100").getFirstName());
+//        System.out.println(eco.getAll());      
+//manualtest DAO getdata
+//        for (Employee employee : edao.getData("")) {
+//            System.out.println(employee.getFirstName()+", "+employee.getLastName());
+//            System.out.println(employee.getDepartmentId().getDepartmentName());
+//        }
+//manualtest DAO getbyid
+//        System.out.println(edao.getById(new Integer(100)).getLastName());
+//manualtest DAO delete        
+//        Employee employee = new Employee(110);
+//        System.out.println(edao.saveOrDelete(employee, true));
+        
+//manualtest DAO insert
+//        String date2 = "21/02/2000";
+//        SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yyyy");
+//        Date date1 = formatter1.parse(date2);
+//
+//        System.out.println(edao.saveOrDelete(new Employee(new Integer(111), "put", "tra", "ppra", "098", date1, new BigDecimal(1000), new BigDecimal(0),
+//                new Department(new Short("90")), new Employee(100), new Job("IT_PROG")), false));
+//    }
+
+//manualtest DAO update
+//            System.out.println(edao.saveOrDelete(new Employee(new Integer(110), "put", "tra", "ppradana92@", "098", date1, new BigDecimal(1000), new BigDecimal(0),
+//                new Department(new Short("90")), new Employee(100), new Job("IT_PROG")), false));
+//    }
+    }
+}
+
+
